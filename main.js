ef552d9459939b8fa5fef45ec6ea535a16fc16c5
murtazaujjainwala786 <86197540+murtazaujjainwala786@users.noreply.github.com>
2021-07-01 17:36:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+name_of_the_people_array - [];
+var guestname = document.getElementById("name1").value;
+name_of_the_people.push(guestname);
+console.log(name_of_the_people);
+
+function sorted()
+name_of_the_people_array.sorted();
+console.log(name_of_the_people_array);
+document.getElementById("display_name").innerHTML = name_of_the_people_array;
+name_of_the_people_array=[];
+
+function sorting()
+{
+var s= document.getElementById("s1").value
+var found=0;
+var j;
+for(j=0; j<name_of_the_people.lenght; j++)
+{
+if(j==name_of_the_people{j)} {
+    found=found+1;
+}
+
+}
+document.getElementById(p2).innerHTML="name found "+found+" time/s"
+console.log("found name "+found+" time/s")
+}
+{
+    var display_student_array=[];
+    for(var j=1;j<=4;j++)
+var name_of_the_student = document.getElementById("name_of_the_student"+j).value
+console.log(name_of_the_student);
+name_of_the_student_array.push(name_of_the_student);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
